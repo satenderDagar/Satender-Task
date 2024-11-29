a0eeb6df233ce4ed101733f37fa403eefd8d3b67
satenderDagar <satender.dagar10@gmail.com>
2024-11-29 17:34:07 +0530

    Initial Commit

---
+++ Satender Task/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Satender Task
+//
+//  Created by Satender Dagar on 29/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Satender TaskTests/Satender_TaskTests.swift
@@ -0,0 +1,17 @@
+//
+//  Satender_TaskTests.swift
+//  Satender TaskTests
+//
+//  Created by Satender Dagar on 29/11/24.
+//
+
+import Testing
+@testable import Satender_Task
+
+struct Satender_TaskTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
